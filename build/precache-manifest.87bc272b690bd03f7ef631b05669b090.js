8931173b059de9569c882c1e5c9914289960ad48
ShaliniMuthukumar <106624891+ShaliniMuthukumar@users.noreply.github.com>
2023-03-05 21:36:03 +0530

    Add files via upload

---
+++ build/precache-manifest.87bc272b690bd03f7ef631b05669b090.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a878a005dc05e66becb6c4e66a40f764",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db908c2386543c5ba33f",
+    "url": "/static/css/main.3ea1df17.chunk.css"
+  },
+  {
+    "revision": "c9c12e3f6c4c32c8822d",
+    "url": "/static/js/2.be80eaf5.chunk.js"
+  },
+  {
+    "revision": "db908c2386543c5ba33f",
+    "url": "/static/js/main.9efd7c0d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
